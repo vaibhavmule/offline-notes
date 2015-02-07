b40d889a7135d62090652aac43839e8435bf9e99
ashwinbalaguru.c@gmail.com <vaibhavmule135@gmail.com>
2015-02-07 22:01:48 +0530

    added notes models

--- notes/rest_notes/models.py
+++ notes/rest_notes/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Note(models.Model):
+    client_id = models.CharField(max_length=100, db_index=True)
+    user = models.ForeignKey(User)
+    note = models.TextField(max_length=1000, db_index=True)
+    created = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
